3ae08fc90e622df4dcab61bdec819a50c6d0bead
arnabpanja <arnab.panja@gmail.com>
2026-05-03 09:44:39 +0000

    Added a tester class for HelloWorldStatic

---
+++ src/main/java/com/arnabp/chap01_test/HelloWorldStaticTester.java
@@ -0,0 +1,12 @@
+package com.arnabp.chap01_test;
+
+import com.arnabp.chap01.HelloWorldStatic;
+
+public class HelloWorldStaticTester {
+    
+    public static void main(String[] args){
+
+        HelloWorldStatic.sayHello();
+
+    }
+}
