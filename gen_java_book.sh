7a450a9b66c445593b3a80d4f89d87b44a60dfd2
arnabpanja <arnab.panja@gmail.com>
2026-05-10 04:32:33 +0000

    scrolling styles added with a script to generate the book indexfile

---
+++ gen_java_book.sh
@@ -0,0 +1,2 @@
+# generate the book 
+jupyter nbconvert --to html '/home/arnabp/projects/simple_java/src/main/java/com/arnabp/SimpleJava.ipynb' --template my_scroll_style --output 'index' --output-dir='/home/arnabp/projects/simple_java'
